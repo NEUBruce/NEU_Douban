d5f31bc5c776a061c18bb901f0bdde2b15b8490a
Bruce Pan <1208313481@qq.com>
2023-09-19 10:15:13 +0800

    modify the pom.xml, remove some outdated API

--- src/main/java/com/rcd/model/MyDataModel.java
+++ src/main/java/com/rcd/model/MyDataModel.java
@@ -3,7 +3,7 @@ package com.rcd.model;
 import org.apache.mahout.cf.taste.impl.model.jdbc.ConnectionPoolDataSource;
 import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
 import org.apache.mahout.cf.taste.model.JDBCDataModel;
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class MyDataModel {
 
@@ -19,7 +19,7 @@ public class MyDataModel {
 
             ConnectionPoolDataSource connectionPool=new ConnectionPoolDataSource(dataSource);
             // use JNDI
-            dataModel = new MySQLJDBCDataModel(connectionPool,"movie_preferences", "userID", "movieID","preference");
+            dataModel = new MySQLJDBCDataModel(connectionPool,"movie_preferences", "userID", "movieID","preference", null);
 
 
         } catch (Exception e) {
