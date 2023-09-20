7a31a2608ac0cd8ce4addde2b796bcd2a71568fc
ligeng <gengli778@gmail.com>
2023-09-20 13:20:33 +0800

    moviemapper后端代码

--- src/main/java/com/neu/mapper/MovieMapper.java
+++ src/main/java/com/neu/mapper/MovieMapper.java
@@ -1,5 +1,23 @@
 package com.neu.mapper;
 
-public interface MovieMapper {
+import com.neu.pojo.Movie;
+import com.neu.pojo.User;
 
+import java.util.List;
+
+public interface MovieMapper {
+    //查询电影列表
+    List<Movie> selectAllMovie();
+
+    //查询用户
+    Movie selectMovieById(Movie movie);
+
+    //创建用户基本信息
+    int addMovie(Movie movie);
+
+    //根据id删除用户信息
+    int deleteMovieById(Movie movie);
+
+    //编辑用户信息
+    int modifyMovie(Movie movie);
 }
