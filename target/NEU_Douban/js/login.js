01223beb1c6a0d01d6ca93ded27ce49184652881
linzhefan <1943465235@qq.com>
2023-09-20 16:57:16 +0800

    qianduan update

---
+++ target/NEU_Douban/js/login.js
@@ -0,0 +1,13 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // 在这里可以添加登录逻辑，例如发送请求到服务器验证用户名和密码
+
+    alert("登录成功！"); // 仅作为示例
+}
+
+function register() {
+    // 在这里可以添加注册逻辑，例如跳转到注册页面
+    alert("跳转到注册页面"); // 仅作为示例
+}
