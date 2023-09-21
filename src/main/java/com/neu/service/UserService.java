ea4ef8e808b99c2bce651fe63e04b567b04fa3c3
ligeng <gengli778@gmail.com>
2023-09-21 14:45:28 +0800

    好友功能

--- src/main/java/com/neu/service/UserService.java
+++ src/main/java/com/neu/service/UserService.java
@@ -50,4 +50,27 @@ public class UserService {
         return mapper.deleteUserByName(user);
     }
 
+    //查找所有关注
+    public List<User> selectAllFriends(User user){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        sqlSession.close();
+        return mapper.selectAllFriends(user);
+    }
+
+    //增加关注
+    public int addFriends(User user,Integer friendId){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        sqlSession.close();
+        return mapper.addFriends(user,friendId);
+    }
+
+    //删除关注
+    public int deleteFriends(User user,Integer friendId){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        sqlSession.close();
+        return mapper.deleteFriends(user,friendId);
+    }
 }
---
+++ src/main/java/com/neu/web/DelFriendsServlet.java
@@ -0,0 +1,65 @@
+package com.neu.web;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.neu.pojo.Movie;
+import com.neu.pojo.User;
+import com.neu.service.MovieService;
+import com.neu.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/addFriends")
+public class DelFriendsServlet extends HttpServlet{
+    private UserService userService = new UserService();
+    private ObjectMapper objectMapper = new ObjectMapper(); // 初始化ObjectMapper
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // 获取用户输入的friendId
+        BufferedReader reader = request.getReader();
+        StringBuilder json = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            json.append(line);
+        }
+
+        // 解析JSON以获取friendId
+        JsonNode jsonNode = objectMapper.readTree(json.toString());
+        String strFriendId = jsonNode.get("friendId").asText();
+        Integer friendId = Integer.parseInt(strFriendId);
+
+        //得到当前登录的user
+        HttpSession session = request.getSession();
+        User currentUser = (User) session.getAttribute("user");
+
+        if(currentUser!=null){
+            Integer delResult=userService.deleteFriends(currentUser,friendId);
+            request.setAttribute("delResult", delResult);
+        }else{
+            throw new RuntimeException("currentUser is null");
+        }
+
+        //页面重定向
+
+    }
+
+}
+
+
