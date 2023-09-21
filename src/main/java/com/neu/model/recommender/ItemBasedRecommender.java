604083ff4a6dbdc72a7031c86296483ef9838104
Bruce Pan <1208313481@qq.com>
2023-09-21 11:29:54 +0800

    remove some bug codes

--- src/main/java/com/neu/model/recommender/ItemBasedRecommender.java
+++ src/main/java/com/neu/model/recommender/ItemBasedRecommender.java
@@ -13,20 +13,6 @@ import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import java.util.List;
 
 public class ItemBasedRecommender {
-    public List<RecommendedItem> getItemBasedRecommender(DataModel dataModel) throws TasteException {
-        // 欧几里得计算物品相似度
-        ItemSimilarity itemSimilarity = new EuclideanDistanceSimilarity(dataModel);
-        RecommenderBuilder recommenderBuilder = RecommendFactory.itemKNNRecommender(itemSimilarity, new NonNegativeQuadraticOptimizer(), 10);
 
-        RecommendFactory.evaluate(RecommendFactory.EVALUATOR.AVERAGE_ABSOLUTE_DIFFERENCE, recommenderBuilder, null, dataModel, 0.7);
-        RecommendFactory.statsEvaluator(recommenderBuilder, null, dataModel, 2);
 
-        LongPrimitiveIterator iter = dataModel.getUserIDs();
-        while (iter.hasNext()) {
-            long uid = iter.nextLong();
-            List list = recommenderBuilder.buildRecommender(dataModel).recommend(uid, RECOMMENDER_NUM);
-            RecommendFactory.showItems(uid, list, true);
-        }
-
-    }
 }
